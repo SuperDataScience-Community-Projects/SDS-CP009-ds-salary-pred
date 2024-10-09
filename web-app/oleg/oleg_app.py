004eedb9ac53711931cacdebc428ac8043447947
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-10-09 17:23:05 +0200

    Update oleg_app.py

--- web-app/oleg/oleg_app.py
+++ web-app/oleg/oleg_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('My First Streamlit App')
+st.title('Streamlit Application')
 
 user_input = st.text_input("Enter some text")
 st.write('You entered: ', user_input)
