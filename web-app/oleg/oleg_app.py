39c9db05076db350a1c761c116bf877f93b6bdaa
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-10-12 00:38:34 +0000

    test

--- web-app/oleg/oleg_app.py
+++ web-app/oleg/oleg_app.py
@@ -10,20 +10,25 @@ def load_model():
         return(loaded_model)
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
+        return None   
     except IOError:
         print(f"Error: An error occurred while trying to read the file '{file_path}'.")
+        return None 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
-# @st.cache_resource
-# def load_companies():
-
-# @st.cache_resource
-# def load_jobs():
-
-# @st.cache_resource
-# def load_locations();    
-
+        return None
+    
+@st.cache_resource
+def load_data(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        return df
+    except FileNotFoundError:
+        st.error(f"Error: The file '{file_path}' was not found.")
+        return None
+    except Exception as e:
+        st.error(f"An unexpected error occurred: {e}")
+        return None
 
 
 # App execution
@@ -34,16 +39,32 @@ def app():
             page_icon='💸', 
             layout='wide'
             )
-
+    #load data
     model = load_model()
+    location_df = load_data('locations.csv')
+    job_df = load_data('jobs.csv')
+    company_df = load_data('companies.csv')
 
+   
+    
     with st.sidebar:
+        #Location
+        location_name = st.selectbox('Location :', location_df['Location'].unique())
+        # Retrieve the corresponding value
+        location_value = location_df[location_df['Location'] == location_name]['Location_Code'].values[0]
+            
+
+
+        #job
+
+        #company
+
         st.selectbox('Location', ["A1",2,3])
         st.selectbox('Job', [1,2,3])
         st.selectbox('Company', [1,2,3])
 
+    st.write = location_value
 
-
-#run application
+#run application 
 if __name__ == "__main__":
     app()
